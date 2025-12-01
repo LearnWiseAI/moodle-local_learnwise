176681ca50408f1172acdd015fc2377e0df50434
Raju Patel <rajuptl21@gmail.com>
2025-12-01 15:29:03 +0530

    Allow admin user token in rest api

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
 $plugin->release      = '1.2.3b';
-$plugin->version      = 2026012300;
+$plugin->version      = 2026012301;
 $plugin->requires     = 2017111309;
 $plugin->supported    = [34, 38];
 $plugin->maturity     = MATURITY_STABLE;
