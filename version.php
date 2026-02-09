a12c350da760b4523fe4564f61d85a18cb75be91
Raju Patel <rajuptl21@gmail.com>
2026-02-09 12:48:58 +0530

    add new function to webservice

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
-$plugin->release      = '1.2.3a';
+$plugin->release      = '1.2.4a';
 $plugin->version      = 2026012301;
 $plugin->requires     = 2020061500;
 $plugin->supported    = [39, 501];
