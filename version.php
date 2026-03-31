7a1d59e3ea592ed6e601d7b43b6435a77555ca7f
Raju Patel <rajuptl21@gmail.com>
2026-03-31 16:05:44 +0530

    fix user permission

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
 $plugin->release      = '1.3.0b';
-$plugin->version      = 2026022600;
+$plugin->version      = 2026033100;
 $plugin->requires     = 2017111309;
 $plugin->supported    = [34, 38];
 $plugin->maturity     = MATURITY_STABLE;
