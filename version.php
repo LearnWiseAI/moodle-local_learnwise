cb87953930eb63cc726a34adde5ea723812c8a1b
Raju Patel <rajuptl21@gmail.com>
2026-06-05 11:53:04 +0530

    add support of marking guide and rubric ranges.
    (cherry picked from commit 64a2ff3af5d8b186acabedf20bb47c79a23a9477)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
-$plugin->release      = '1.3.8b';
-$plugin->version      = 2026051500;
+$plugin->release      = '1.3.9b';
+$plugin->version      = 2026060400;
 $plugin->requires     = 2017111309;
 $plugin->supported    = [34, 38];
 $plugin->maturity     = MATURITY_STABLE;
