9c3bdcd9452f26c0e57e576293bb47bc6272812c
Raju Patel <rajuptl21@gmail.com>
2026-07-21 13:29:33 +0530

    new endpoints: sections, files and discussions

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
-$plugin->release      = '1.4.1a';
-$plugin->version      = 2026071700;
+$plugin->release      = '1.4.2a';
+$plugin->version      = 2026072100;
 $plugin->requires     = 2020061500;
 $plugin->supported    = [39, 502];
 $plugin->maturity     = MATURITY_STABLE;
