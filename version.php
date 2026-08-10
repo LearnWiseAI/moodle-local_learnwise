7bd22b85ffa42b266b7595c3c943d61ec2fb1aee
Raju Patel <rajuptl21@gmail.com>
2026-08-10 14:22:19 +0530

    update version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
-$plugin->release      = '1.4.4a';
-$plugin->version      = 2026073000;
+$plugin->release      = '1.4.5a';
+$plugin->version      = 2026081000;
 $plugin->requires     = 2020061500;
 $plugin->supported    = [39, 502];
 $plugin->maturity     = MATURITY_STABLE;
