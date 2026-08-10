0c4597b7d9ac5aa431e165375f091e4805ac3c21
Raju Patel <rajuptl21@gmail.com>
2026-08-10 17:39:12 +0530

    quiz endpoint change format to float

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_learnwise';
-$plugin->release      = '1.4.5b';
-$plugin->version      = 2026080900;
+$plugin->release      = '1.4.6b';
+$plugin->version      = 2026080901;
 $plugin->requires     = 2017111309;
 $plugin->supported    = [34, 38];
 $plugin->maturity     = MATURITY_STABLE;
